4ea21167070e2904a9c741535a62355a3615e209
sunydaisy <sunydaisy@163.com>
2019-05-05 18:27:28 +0800

    11

---
+++ src/main/java/com/ocreatech/pms/entity/basic/ResponseDataVO.java
@@ -0,0 +1,14 @@
+package com.ocreatech.pms.entity.basic;
+
+
+public class ResponseDataVO<T> {
+	
+	private Boolean success;
+	
+	private Integer status;
+	
+	private String msg;
+	
+	private T data;
+
+}
