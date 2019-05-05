a6926382b22b00bbc9fb2315f0d3f9a7546cd360
sunydaisy <sunydaisy@163.com>
2019-05-05 16:12:37 +0800

    pms-api

---
+++ src/main/java/com/ocreatech/pms/PmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ocreatech.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsApiApplication.class, args);
+	}
+
+}
